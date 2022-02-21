850823d57156fdd591bb84b10de1681cc8d3d24e
한성희 <bee1404@naver.com>
2022-02-22 00:24:38 +0900

    Level 1 숫자 문자열과 영단어 Solution

---
+++ Level 1/Lv1_숫자문자열과영단어.java
@@ -0,0 +1,20 @@
+package programmers;
+
+public class Lv1_숫자문자열과영단어 {
+	class Solution {
+	    public int solution(String s) {
+	        s = s.replaceAll("zero", "0");
+	        s = s.replaceAll("one", "1");
+	        s = s.replaceAll("two", "2");
+	        s = s.replaceAll("three", "3");
+	        s = s.replaceAll("four", "4");
+	        s = s.replaceAll("five", "5");
+	        s = s.replaceAll("six", "6");
+	        s = s.replaceAll("seven", "7");
+	        s = s.replaceAll("eight", "8");
+	        s = s.replaceAll("nine", "9");
+	    
+	        return Integer.parseInt(s);
+	    }
+	}
+}
