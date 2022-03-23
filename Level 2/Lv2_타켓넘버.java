69955ab9f4712c04285633f9cd006f1d05da3c9d
한성희 <bee1404@naver.com>
2022-03-23 21:46:04 +0900

    Level 2 타겟 넘버 Solution

---
+++ Level 2/Lv2_타켓넘버.java
@@ -0,0 +1,25 @@
+package programmers;
+
+public class Lv2_타켓넘버 {
+	static int answer;
+
+	class Solution {
+	    
+	    public int solution(int[] numbers, int target) {
+	        answer = 0;
+	        
+	        dfs(numbers, target, 0, 0);
+	        return answer;
+	    }
+	    
+	    public void dfs(int[] numbers, int target, int idx, int sum) {
+	        if(idx==numbers.length) {
+	            if(sum == target) answer++;
+	            return;
+	        }
+	        
+	        dfs(numbers, target, idx+1, sum+numbers[idx]);
+	        dfs(numbers, target, idx+1, sum-numbers[idx]);
+	    }
+	}
+}
